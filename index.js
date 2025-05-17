161b6e9bca5f74bdddccfdd65d1172f261765775
Marco Guadiana <marcoguadiana01@gmail.com>
2025-05-17 14:31:03 -0500

    WHOOOHOOO

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+const burgers = ['Hamburger', 'Cheeseburger']; 
+
+let featuredDrink = 'Strawberry Milkshake'; 
+
+function addBurger()  {
+    const newBurger = ['Flatburger']; 
+    burgers.push(newBurger); 
+
+    if (1 ==  1) {
+        const anotherNewBurger = 'Maple Bacon Burger' ;
+        burgers.push(anotherNewBurger);
+
+    }
+    
+
+}
+
+function changeFeaturedDrink() {
+    featuredDrink = 'The JavaShake'; 
+}
+
+
+console.log(featuredDrink);
+changeFeaturedDrink(); 
+console.log(featuredDrink);
